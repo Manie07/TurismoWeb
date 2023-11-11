2a3060f973f4a3b51986fe3b8cde60b841431890
lucho <luiscaraballo31917128@gmail.com>
2023-11-10 23:01:25 -0500

    registrar añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioTuristaComponent } from './inicio-turista/inicio-turista.component';
 import { IniciarSecionTuristaComponent } from './iniciar-secion-turista/iniciar-secion-turista.component';
+import { RegistrarTuristaComponent } from './registrar-turista/registrar-turista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioTuristaComponent,
-    IniciarSecionTuristaComponent
+    IniciarSecionTuristaComponent,
+    RegistrarTuristaComponent
   ],
   imports: [
     BrowserModule,
