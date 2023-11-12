420b06f05294a464252705d9a078cbf42d3d0013
manie07 <emanuelcacerescab7779@gmail.com>
2023-11-11 23:12:37 -0500

    tuneo del home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { InicioTuristaComponent } from './inicio-turista/inicio-turista.component';
 import { IniciarSecionTuristaComponent } from './iniciar-secion-turista/iniciar-secion-turista.component';
 import { RegistrarTuristaComponent } from './registrar-turista/registrar-turista.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioTuristaComponent,
     IniciarSecionTuristaComponent,
-    RegistrarTuristaComponent
+    RegistrarTuristaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(), 
+    ButtonsModule.forRoot(),
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
