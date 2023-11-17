5db144987c5e5f66bb686331cef8a5d42c73bacd
manie07 <emanuelcacerescab7779@gmail.com>
2023-11-17 18:08:25 -0500

    Algo mas

---
+++ src/app/registrar-turista/registrar-turista.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-turista',
+  templateUrl: './registrar-turista.component.html',
+  styleUrls: ['./registrar-turista.component.css']
+})
+export class RegistrarTuristaComponent {
+
+}
