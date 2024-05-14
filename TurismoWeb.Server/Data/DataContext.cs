d708feb1a3bf82cb7cf180677e310d36c57d6b2d
luis <luis.alvarezpareja@unicolombo.edu.co>
2024-05-14 07:00:12 -0500

    apis crud ejemplo

---
+++ TurismoWeb.Server/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TurismoWeb.Server.Entities;
+
+namespace TurismoWeb.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions<DataContext> options ) : base(options)
+            {
+
+
+
+            }
+
+        public DbSet<Usuario> usuariosprueba { get; set; }
+    }
+}
---
+++ TurismoWeb.Server/Migrations/20240422023232_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TurismoWeb.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuariosprueba",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrimerNombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lugar = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
