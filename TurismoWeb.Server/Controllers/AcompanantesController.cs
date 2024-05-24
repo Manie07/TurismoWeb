e737b5fa3159ff8286aa2130bb8797f012ef5def
luis <luis.alvarezpareja@unicolombo.edu.co>
2024-05-24 00:59:40 -0500

    controlladores actualizados

---
+++ TurismoWeb.Server/Controllers/AcompanantesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TurismoWeb.Server.Data;
+using TurismoWeb.Server.Entities;
+
+namespace TurismoWeb.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcompanantesController : ControllerBase
+    {
+        private readonly TurismowebContext _context;
+
+        public AcompanantesController(TurismowebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Acompanantes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Acompanante>>> GetAcompanantes()
+        {
+            return await _context.Acompanantes.ToListAsync();
+        }
+
+        // GET: api/Acompanantes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Acompanante>> GetAcompanante(string id)
+        {
+            var acompanante = await _context.Acompanantes.FindAsync(id);
+
+            if (acompanante == null)
+            {
+                return NotFound();
+            }
+
+            return acompanante;
+        }
+
+        // PUT: api/Acompanantes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAcompanante(string id, Acompanante acompanante)
+        {
+            if (id != acompanante.Dni)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(acompanante).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AcompananteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Acompanantes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Acompanante>> PostAcompanante(Acompanante acompanante)
+        {
+            _context.Acompanantes.Add(acompanante);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (AcompananteExists(acompanante.Dni))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetAcompanante", new { id = acompanante.Dni }, acompanante);
+        }
+
+        // DELETE: api/Acompanantes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAcompanante(string id)
+        {
+            var acompanante = await _context.Acompanantes.FindAsync(id);
+            if (acompanante == null)
+            {
+                return NotFound();
+            }
+
+            _context.Acompanantes.Remove(acompanante);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AcompananteExists(string id)
+        {
+            return _context.Acompanantes.Any(e => e.Dni == id);
+        }
+    }
+}
---
+++ TurismoWeb.Server/Controllers/GuiasturistasController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TurismoWeb.Server.Data;
+using TurismoWeb.Server.Entities;
+
+namespace TurismoWeb.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuiasturistasController : ControllerBase
+    {
+        private readonly TurismowebContext _context;
+
+        public GuiasturistasController(TurismowebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Guiasturistas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Guiasturista>>> GetGuiasturistas()
+        {
+            return await _context.Guiasturistas.ToListAsync();
+        }
+
+        // GET: api/Guiasturistas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Guiasturista>> GetGuiasturista(int id)
+        {
+            var guiasturista = await _context.Guiasturistas.FindAsync(id);
+
+            if (guiasturista == null)
+            {
+                return NotFound();
+            }
+
+            return guiasturista;
+        }
+
+        // PUT: api/Guiasturistas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGuiasturista(int id, Guiasturista guiasturista)
+        {
+            if (id != guiasturista.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(guiasturista).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GuiasturistaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Guiasturistas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Guiasturista>> PostGuiasturista(Guiasturista guiasturista)
+        {
+            _context.Guiasturistas.Add(guiasturista);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGuiasturista", new { id = guiasturista.Id }, guiasturista);
+        }
+
+        // DELETE: api/Guiasturistas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGuiasturista(int id)
+        {
+            var guiasturista = await _context.Guiasturistas.FindAsync(id);
+            if (guiasturista == null)
+            {
+                return NotFound();
+            }
+
+            _context.Guiasturistas.Remove(guiasturista);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GuiasturistaExists(int id)
+        {
+            return _context.Guiasturistas.Any(e => e.Id == id);
+        }
+    }
+}
